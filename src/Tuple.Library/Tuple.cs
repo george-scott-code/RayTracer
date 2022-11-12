41cdfe02dcdd4b1dd666a71ed193b3a0f5f30cb5
george.scott <george.scott@sage.com>
2022-11-12 11:46:02 +0000

    add tuple class

---
+++ src/Tuple.Library/Tuple.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TupleLibrary
+{
+    public class Tuple
+    {
+        public Tuple(double x, double y, double w)
+        {
+            X = x;
+            Y = y;
+            W = w;
+        }
+
+        public double X { get; init;}
+        public double Y { get; init;}
+        public double W { get; init;}
+
+        public bool IsPoint => this.W == 1.0;
+        public bool IsVector => this.W == 0.0;
+    }
+}
