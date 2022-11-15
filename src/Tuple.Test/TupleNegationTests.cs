d98a95bbdce23b0126da09306be32211fc34484d
george.scott <george.scott.code@gmail.com>
2022-11-15 19:23:39 +0000

    tuple negation test file

---
+++ src/Tuple.Test/TupleNegationTests.cs
@@ -0,0 +1,22 @@
+using Xunit;
+using TupleLibrary;
+
+namespace TupleTests;
+
+public class TupleNegationTests
+{
+    [Fact]
+    public void Subtracting_a_Vector_from_a_zero_vector()
+    {
+        Tuple t1 = Tuple.Vector(0, 0, 0);
+        Tuple t2 = Tuple.Vector(1, -2, 3);
+
+        var result = t1.Subtract(t2);
+
+        var expected = new Tuple(-1, 2, -3, 0);
+        Assert.Equal(expected, result);
+        Assert.False(result.IsPoint);
+        Assert.True(result.IsVector);
+    }
+
+}
