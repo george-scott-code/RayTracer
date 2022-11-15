131f7e36fe238f1b17d65585d80d7fca08eaa450
george.scott <george.scott.code@gmail.com>
2022-11-15 18:00:41 +0000

    file scopes namespaces

--- src/Tuple.Library/Tuple.cs
+++ src/Tuple.Library/Tuple.cs
@@ -1,66 +1,65 @@
 ﻿using System;
 using TupleLibrary.Extensions;
 
-namespace TupleLibrary
+namespace TupleLibrary;
+
+public class Tuple
 {
-    public class Tuple
+    public Tuple(double x, double y, double z, double w)
     {
-        public Tuple(double x, double y, double z, double w)
-        {
-            X = x;
-            Y = y;
-            Z = z;
-            W = w;
-        }
+        X = x;
+        Y = y;
+        Z = z;
+        W = w;
+    }
 
-        public double X { get; init;}
-        public double Y { get; init;}
-        public double Z { get; init; }
-        public double W { get; init;}
+    public double X { get; init;}
+    public double Y { get; init;}
+    public double Z { get; init; }
+    public double W { get; init;}
 
-        public bool IsPoint => this.W == 1.0;
-        public bool IsVector => this.W == 0.0;
+    public bool IsPoint => this.W == 1.0;
+    public bool IsVector => this.W == 0.0;
 
-        public static Tuple Vector(double x, double y, double z)
-        {
-            return new Tuple(x, y, z, 0.0);
-        }
+    public static Tuple Vector(double x, double y, double z)
+    {
+        return new Tuple(x, y, z, 0.0);
+    }
 
-        public static Tuple Point(double x, double y, double z)
-        {
-            return new Tuple(x, y, z, 1.0);
-        }
-        
-        public Tuple Add(Tuple t2)
-        {
-            return new Tuple(this.X + t2.X, this.Y + t2.Y, this.Z + t2.Z, this.W + t2.W);
-        }
+    public static Tuple Point(double x, double y, double z)
+    {
+        return new Tuple(x, y, z, 1.0);
+    }
+    
+    public Tuple Add(Tuple t2)
+    {
+        return new Tuple(this.X + t2.X, this.Y + t2.Y, this.Z + t2.Z, this.W + t2.W);
+    }
 
-        public Tuple Subtract(Tuple t2)
-        {
-            return new Tuple(this.X - t2.X, this.Y - t2.Y, this.Z - t2.Z,  this.W - t2.W);
-        }
+    public Tuple Subtract(Tuple t2)
+    {
+        return new Tuple(this.X - t2.X, this.Y - t2.Y, this.Z - t2.Z,  this.W - t2.W);
+    }
 
-        public override bool Equals(object obj)
+    public override bool Equals(object obj)
+    {
+        if(obj is null != this is null)
         {
-            if(obj is null != this is null)
-            {
-                return false;
-            }
-            if(obj is null && this is null)
-            {
-                return true;
-            }
-            return obj is Tuple tuple &&
-                   this.X.DecimalEquals(tuple.X) &&
-                   this.Y == tuple.Y &&
-                   this.Z == tuple.Z &&
-                   this.W == tuple.W;
+            return false;
         }
+        if(obj is null && this is null)
+        {
+            return true;
+        }
+        return obj is Tuple tuple &&
+               this.X.DecimalEquals(tuple.X) &&
+               this.Y == tuple.Y &&
+               this.Z == tuple.Z &&
+               this.W == tuple.W;
+    }
 
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(X, Y, Z, W);
-        }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y, Z, W);
     }
 }
--- src/Tuple.Test/TupleTests.cs
+++ src/Tuple.Test/TupleTests.cs
@@ -1,204 +1,203 @@
 using Xunit;
 using TupleLibrary;
 
-namespace TupleTests
+namespace TupleTests;
+
+public class TupleTests
 {
-    public class TupleTests
+    // creation
+    [Fact]
+    public void Tuple_WithW1_IsPoint()
     {
-        // creation
-        [Fact]
-        public void Tuple_WithW1_IsPoint()
-        {
-            Tuple tuple = new Tuple(4.3, -4.2, 3.1, 1.0);
+        Tuple tuple = new Tuple(4.3, -4.2, 3.1, 1.0);
 
-            Assert.Equal(4.3, tuple.X);
-            Assert.Equal(-4.2, tuple.Y);
-            Assert.Equal(3.1, tuple.Z);
-            Assert.Equal(1.0, tuple.W);
-            Assert.True(tuple.IsPoint);
-            Assert.False(tuple.IsVector);
-        }
+        Assert.Equal(4.3, tuple.X);
+        Assert.Equal(-4.2, tuple.Y);
+        Assert.Equal(3.1, tuple.Z);
+        Assert.Equal(1.0, tuple.W);
+        Assert.True(tuple.IsPoint);
+        Assert.False(tuple.IsVector);
+    }
 
-        [Fact]
-        public void Tuple_WithW0_IsVector()
-        {
-            Tuple tuple = new Tuple(4.3, -4.2, 3.1,  0.0);
+    [Fact]
+    public void Tuple_WithW0_IsVector()
+    {
+        Tuple tuple = new Tuple(4.3, -4.2, 3.1,  0.0);
 
-            Assert.Equal(4.3, tuple.X);
-            Assert.Equal(-4.2, tuple.Y);
-            Assert.Equal(3.1, tuple.Z);
-            Assert.Equal(0.0, tuple.W);
-            Assert.False(tuple.IsPoint);
-            Assert.True(tuple.IsVector);
-        }
+        Assert.Equal(4.3, tuple.X);
+        Assert.Equal(-4.2, tuple.Y);
+        Assert.Equal(3.1, tuple.Z);
+        Assert.Equal(0.0, tuple.W);
+        Assert.False(tuple.IsPoint);
+        Assert.True(tuple.IsVector);
+    }
 
-        [Fact]
-        public void Tuple_WithPointConstructor_IsPoint()
-        {
-            Tuple tuple = Tuple.Point(4.3, -4.2, 3.1);
-            
-            Assert.Equal(4.3, tuple.X);
-            Assert.Equal(-4.2, tuple.Y);
-            Assert.Equal(3.1, tuple.Z);
-            Assert.Equal(1.0, tuple.W);
-            Assert.True(tuple.IsPoint);
-            Assert.False(tuple.IsVector);
-        }
-
-        [Fact]
-        public void Tuple_WithPointConstructor_IsVector()
-        {
-            Tuple tuple = Tuple.Vector(4.3, -4.2, 3.1);
-            
-            Assert.Equal(4.3, tuple.X);
-            Assert.Equal(-4.2, tuple.Y);
-            Assert.Equal(3.1, tuple.Z);
-            Assert.Equal(0.0, tuple.W);
-            Assert.False(tuple.IsPoint);
-            Assert.True(tuple.IsVector);
-        }
-
-        // Equality
-        [Fact]
-        public void Point_EqualsVector_IsNotEqual()
-        {
-            Tuple vector = Tuple.Vector(4.3, -4.2, 3.1);
-            Tuple point = Tuple.Point(4.3, -4.2, 3.1);
-
-            Assert.False(point.Equals(vector));
-            Assert.False(vector.Equals(point));
-        }
-
-        [Fact]
-        public void Tuple_EqualsNullTuple_IsNotEqual()
-        {
-            Tuple t1 = new Tuple(4.3, -4.2, 3.1, 1.0);
-            Tuple t2 = (Tuple) null;
-
-            Assert.False(t1.Equals(t2));
-        }
-
-        [Fact]
-        public void Tuples_WithSameValues_AreEqual()
-        {
-            Tuple t1 = new Tuple(4.3, -4.2, 3.1, 1.0);
-            Tuple t2 = new Tuple(4.3, -4.2, 3.1, 1.0);
-
-            Assert.True(t1.Equals(t2));
-            Assert.True(t2.Equals(t1));
-        }
-
-        [Fact]
-        public void Tuples_WithSameValuesWithinPoint00001_AreEqual()
-        {
-            double epsilon = 0.00001;
-            Tuple t1 = new Tuple(4.3 + epsilon, -4.2, 3.1, 1.0);
-            Tuple t2 = new Tuple(4.3, -4.2, 3.1, 1.0);
-
-            Assert.True(t1.Equals(t2));
-            Assert.True(t2.Equals(t1));
-        }
-
-        //Addition
-
-        [Fact]
-        public void Point_AddVector_EqualsPoint()
-        {
-            Tuple t1 = Tuple.Point(1, 2, 3);
-            Tuple t2 = Tuple.Vector(1, 2, 3);
-
-            var result = t1.Add(t2);
-
-            var expected = new Tuple(2, 4, 6, 1);
-            Assert.Equal(expected, result);
-            Assert.True(result.IsPoint);
-        }
-
-        [Fact]
-        public void Vector_AddVector_EqualsVector()
-        {
-            Tuple t1 = Tuple.Vector(1, 2, 3);
-            Tuple t2 = Tuple.Vector(1, 2, 3);
-
-            var result = t1.Add(t2);
-
-            var expected = new Tuple(2,4, 6, 0);
-            Assert.Equal(expected, result);
-            Assert.True(result.IsVector);
-        }
+    [Fact]
+    public void Tuple_WithPointConstructor_IsPoint()
+    {
+        Tuple tuple = Tuple.Point(4.3, -4.2, 3.1);
         
-        // TODO: should we handle this?
-        [Fact]
-        public void Point_AddPoint_EqualsSomething()
-        {
-            Tuple t1 = Tuple.Point(1, 2, 3);
-            Tuple t2 = Tuple.Point(1, 2, 3);
+        Assert.Equal(4.3, tuple.X);
+        Assert.Equal(-4.2, tuple.Y);
+        Assert.Equal(3.1, tuple.Z);
+        Assert.Equal(1.0, tuple.W);
+        Assert.True(tuple.IsPoint);
+        Assert.False(tuple.IsVector);
+    }
 
-            var result = t1.Add(t2);
+    [Fact]
+    public void Tuple_WithPointConstructor_IsVector()
+    {
+        Tuple tuple = Tuple.Vector(4.3, -4.2, 3.1);
+        
+        Assert.Equal(4.3, tuple.X);
+        Assert.Equal(-4.2, tuple.Y);
+        Assert.Equal(3.1, tuple.Z);
+        Assert.Equal(0.0, tuple.W);
+        Assert.False(tuple.IsPoint);
+        Assert.True(tuple.IsVector);
+    }
 
-            var expected = new Tuple(2, 4, 6, 2);
-            Assert.Equal(expected, result);
-            Assert.False(result.IsPoint);
-            Assert.False(result.IsVector);
-        }
+    // Equality
+    [Fact]
+    public void Point_EqualsVector_IsNotEqual()
+    {
+        Tuple vector = Tuple.Vector(4.3, -4.2, 3.1);
+        Tuple point = Tuple.Point(4.3, -4.2, 3.1);
 
-        // Subtraction
+        Assert.False(point.Equals(vector));
+        Assert.False(vector.Equals(point));
+    }
 
-        [Fact]
-        public void SubrtractingTwoPoints()
-        {
-            Tuple t1 = Tuple.Point(3, 2, 7);
-            Tuple t2 = Tuple.Point(5, 6, 1);
+    [Fact]
+    public void Tuple_EqualsNullTuple_IsNotEqual()
+    {
+        Tuple t1 = new Tuple(4.3, -4.2, 3.1, 1.0);
+        Tuple t2 = (Tuple) null;
 
-            var result = t1.Subtract(t2);
+        Assert.False(t1.Equals(t2));
+    }
 
-            var expected = new Tuple(-2, -4, 6, 0);
-            Assert.Equal(expected, result);
-            Assert.False(result.IsPoint);
-            Assert.True(result.IsVector);
-        }
+    [Fact]
+    public void Tuples_WithSameValues_AreEqual()
+    {
+        Tuple t1 = new Tuple(4.3, -4.2, 3.1, 1.0);
+        Tuple t2 = new Tuple(4.3, -4.2, 3.1, 1.0);
 
-        [Fact]
-        public void SubrtractingVectorFromPoint()
-        {
-            Tuple t1 = Tuple.Point(3, 2, 7);
-            Tuple t2 = Tuple.Vector(5, 6, 1);
+        Assert.True(t1.Equals(t2));
+        Assert.True(t2.Equals(t1));
+    }
 
-            var result = t1.Subtract(t2);
+    [Fact]
+    public void Tuples_WithSameValuesWithinPoint00001_AreEqual()
+    {
+        double epsilon = 0.00001;
+        Tuple t1 = new Tuple(4.3 + epsilon, -4.2, 3.1, 1.0);
+        Tuple t2 = new Tuple(4.3, -4.2, 3.1, 1.0);
 
-            var expected = new Tuple(-2, -4, 6, 1);
-            Assert.Equal(expected, result);
-            Assert.True(result.IsPoint);
-            Assert.False(result.IsVector);
-        }
+        Assert.True(t1.Equals(t2));
+        Assert.True(t2.Equals(t1));
+    }
 
-        [Fact]
-        public void SubrtractingTwoVectors()
-        {
-            Tuple t1 = Tuple.Vector(3, 2, 7);
-            Tuple t2 = Tuple.Vector(5, 6, 1);
+    //Addition
 
-            var result = t1.Subtract(t2);
+    [Fact]
+    public void Point_AddVector_EqualsPoint()
+    {
+        Tuple t1 = Tuple.Point(1, 2, 3);
+        Tuple t2 = Tuple.Vector(1, 2, 3);
 
-            var expected = new Tuple(-2, -4, 6, 0);
-            Assert.Equal(expected, result);
-            Assert.False(result.IsPoint);
-            Assert.True(result.IsVector);
-        }
+        var result = t1.Add(t2);
 
-        // TODO: should we handle this?
-        [Fact]
-        public void SubrtractingPointFromVector()
-        {
-            Tuple t1 = Tuple.Vector(3, 2, 7);
-            Tuple t2 = Tuple.Point(5, 6, 1);
+        var expected = new Tuple(2, 4, 6, 1);
+        Assert.Equal(expected, result);
+        Assert.True(result.IsPoint);
+    }
 
-            var result = t1.Subtract(t2);
+    [Fact]
+    public void Vector_AddVector_EqualsVector()
+    {
+        Tuple t1 = Tuple.Vector(1, 2, 3);
+        Tuple t2 = Tuple.Vector(1, 2, 3);
 
-            var expected = new Tuple(-2, -4, 6, -1);
-            Assert.Equal(expected, result);
-            Assert.False(result.IsPoint);
-            Assert.False(result.IsVector);
-        }
+        var result = t1.Add(t2);
+
+        var expected = new Tuple(2,4, 6, 0);
+        Assert.Equal(expected, result);
+        Assert.True(result.IsVector);
+    }
+    
+    // TODO: should we handle this?
+    [Fact]
+    public void Point_AddPoint_EqualsSomething()
+    {
+        Tuple t1 = Tuple.Point(1, 2, 3);
+        Tuple t2 = Tuple.Point(1, 2, 3);
+
+        var result = t1.Add(t2);
+
+        var expected = new Tuple(2, 4, 6, 2);
+        Assert.Equal(expected, result);
+        Assert.False(result.IsPoint);
+        Assert.False(result.IsVector);
+    }
+
+    // Subtraction
+
+    [Fact]
+    public void SubrtractingTwoPoints()
+    {
+        Tuple t1 = Tuple.Point(3, 2, 7);
+        Tuple t2 = Tuple.Point(5, 6, 1);
+
+        var result = t1.Subtract(t2);
+
+        var expected = new Tuple(-2, -4, 6, 0);
+        Assert.Equal(expected, result);
+        Assert.False(result.IsPoint);
+        Assert.True(result.IsVector);
+    }
+
+    [Fact]
+    public void SubrtractingVectorFromPoint()
+    {
+        Tuple t1 = Tuple.Point(3, 2, 7);
+        Tuple t2 = Tuple.Vector(5, 6, 1);
+
+        var result = t1.Subtract(t2);
+
+        var expected = new Tuple(-2, -4, 6, 1);
+        Assert.Equal(expected, result);
+        Assert.True(result.IsPoint);
+        Assert.False(result.IsVector);
+    }
+
+    [Fact]
+    public void SubrtractingTwoVectors()
+    {
+        Tuple t1 = Tuple.Vector(3, 2, 7);
+        Tuple t2 = Tuple.Vector(5, 6, 1);
+
+        var result = t1.Subtract(t2);
+
+        var expected = new Tuple(-2, -4, 6, 0);
+        Assert.Equal(expected, result);
+        Assert.False(result.IsPoint);
+        Assert.True(result.IsVector);
+    }
+
+    // TODO: should we handle this?
+    [Fact]
+    public void SubrtractingPointFromVector()
+    {
+        Tuple t1 = Tuple.Vector(3, 2, 7);
+        Tuple t2 = Tuple.Point(5, 6, 1);
+
+        var result = t1.Subtract(t2);
+
+        var expected = new Tuple(-2, -4, 6, -1);
+        Assert.Equal(expected, result);
+        Assert.False(result.IsPoint);
+        Assert.False(result.IsVector);
     }
 }
