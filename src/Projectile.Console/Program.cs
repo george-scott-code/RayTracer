f0553cffd237c0ac1a26087bb3d1b99cd539a536
george.scott <george.scott.code@gmail.com>
2022-11-26 13:01:03 +0000

    clean up references

--- src/Projectile.Console/Program.cs
+++ src/Projectile.Console/Program.cs
@@ -1,24 +1,25 @@
-﻿using System;
+﻿using TupleLibrary;
+using Sys=System;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        var environment = new Environment(TupleLibrary.Tuple.Vector(0, -0.1, 0), TupleLibrary.Tuple.Vector(-0.01, 0, 0));
-        var projectile = new Projectile(TupleLibrary.Tuple.Point(0, 1, 0), TupleLibrary.Tuple.Vector(1, 1, 0).Normalize());
+        var environment = new Environment(Tuple.Vector(0, -0.1, 0), Tuple.Vector(-0.01, 0, 0));
+        var projectile = new Projectile(Tuple.Point(0, 1, 0), Tuple.Vector(1, 1, 0).Normalize());
 
         while(projectile.Position.Y >= 0.0)
         {
-            projectile = Tick(environment, projectile);
-            System.Console.WriteLine($"x position: {projectile.Position.X}");
+            projectile = Update(environment, projectile);
+            Sys.Console.WriteLine($"x position: {projectile.Position.X}");
         }
     }
 
-    private static Projectile Tick(Environment env, Projectile proj)
+    private static Projectile Update(Environment env, Projectile proj)
     {
-        var position = proj.Position.Add(proj.Velocity);
-        var velocity = proj.Velocity.Add(env.Wind).Add(env.Gravity);
-        return new Projectile(position, velocity);
+        return new Projectile(
+            proj.Position.Add(proj.Velocity),
+            proj.Velocity.Add(env.Wind).Add(env.Gravity)
+        );
     }
 }
