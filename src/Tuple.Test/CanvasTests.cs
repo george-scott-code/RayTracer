3e475ab6aba671f42c12cf279dcab6b44e5e525c
george.scott <george.scott.code@gmail.com>
2022-11-30 19:00:05 +0000

    add test for ppm pixel data

--- src/Tuple.Test/CanvasTests.cs
+++ src/Tuple.Test/CanvasTests.cs
@@ -39,11 +39,10 @@ public class CanvasTests
     [Fact]
     public void Constructing_the_PPM_header()
     {
-        // Given
         Canvas c = new Canvas(5, 3);
-        // When
+
         string ppm = c.ToPPM();
-        // Then
+
         using var reader = new StringReader(ppm);
         string firstLine = reader.ReadLine();
         string secondLine = reader.ReadLine();
@@ -52,5 +51,33 @@ public class CanvasTests
         Assert.Equal("P3", firstLine);
         Assert.Equal("5 3", secondLine);
         Assert.Equal("255", thirdLine);
+    }
+
+    [Fact]
+    public void Constructing_the_PPM_pixel_data()
+    {
+        Canvas c = new Canvas(5, 3);
+        var c1 = new Color(1.5,0,0);
+        var c2 = new Color(1,0.5,0);
+        var c3 = new Color(-0.5,0,1);
+
+
+        c.WritePixel(0, 0, c1);
+        c.WritePixel(2, 1, c2);
+        c.WritePixel(4, 2, c3);
+
+        string ppm = c.ToPPM();
+
+        using var reader = new StringReader(ppm);
+        _ = reader.ReadLine();
+        _ = reader.ReadLine();
+        _ = reader.ReadLine();
+        string line4 = reader.ReadLine();
+        string line5 = reader.ReadLine();
+        string line6 = reader.ReadLine();
+
+        Assert.Equal("255 0 0 0 0 0 0 0 0 0 0 0 0 0 0", line4);
+        Assert.Equal("0 0 0 0 0 0 0 128 0 0 0 0 0 0 0", line5);
+        Assert.Equal("0 0 0 0 0 0 0 0 0 0 0 0 0 0 255", line6);
     }
 }
