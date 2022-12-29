76582056369181fcb33bdbdf97dfea716f5c0684
george-scott-code <george.scott.code@gmail.com>
2022-12-29 10:42:38 +0000

    format

--- src/Tuple.Test/CanvasTests.cs
+++ src/Tuple.Test/CanvasTests.cs
@@ -121,15 +121,13 @@ public class CanvasTests
             var c2 = new Color(0, 0.5, 0);
             var c3 = new Color(-0.6, 0, 1);
 
-
             c.WritePixel(0, 0, c1);
             c.WritePixel(2, 1, c2);
             c.WritePixel(4, 2, c3);
 
             string ppm = c.ToPPM();
-
             File.WriteAllText("saved.ppm", ppm);
-            //Then
+
             Assert.True(File.Exists("saved.ppm"));
         }
 }
