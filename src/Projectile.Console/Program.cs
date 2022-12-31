fa6a9f8a72bd3325e1cf5415317f06d7b53fcfd1
george-scott-code <george.scott.code@gmail.com>
2022-12-31 16:08:20 +0000

    draw 2 trajectories

--- src/Projectile.Console/Program.cs
+++ src/Projectile.Console/Program.cs
@@ -30,13 +30,18 @@ internal class Program
         //The projectile’s velocity was normalized to a unit vector, and then multiplied
         //by 11.25 to increase its magnitude.
         var velo = Tuple.Vector(1, 1.8, 0.0).Normalize() * 11.25;
+        var velo2 = Tuple.Vector(1, 0.9, 0.0).Normalize() * 9.5;
+
         var proj = new Projectile(start, velo);
+        var proj2 = new Projectile(start, velo2);
 
         var grav = Tuple.Vector(0, -0.1, 0);
         var win = Tuple.Vector(-0.01, 0, 0);
 
         var env = new Environment(grav, win);
         var red = new Color(1.5, 0, 0);
+        var blue = new Color(0, 0, 1.5);
+
 
         while(proj.Position.Y >= 0.0)
         {
@@ -46,6 +51,16 @@ internal class Program
                 proj.Position.Y >= 0 &&
                 proj.Position.Y <= 550)
             c.WritePixel((int)proj.Position.X, (550 - (int)proj.Position.Y), red);
+        }
+
+        while(proj2.Position.Y >= 0.0)
+        {
+            proj2 = Update(env, proj2);
+            if(proj2.Position.X >= 0 && 
+                proj2.Position.X <= 900 &&
+                proj2.Position.Y >= 0 &&
+                proj2.Position.Y <= 550)
+            c.WritePixel((int)proj2.Position.X, (550 - (int)proj2.Position.Y), blue);
         }
         var ppm = c.ToPPM();
         File.WriteAllText("saved.ppm", ppm);
