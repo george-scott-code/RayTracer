0fb1b5a03a51fe0fcbc6520c810d3c6f14ae30a8
george-scott-code <george.scott.code@gmail.com>
2022-12-31 15:50:50 +0000

    test saving to canvas

--- src/Projectile.Console/Program.cs
+++ src/Projectile.Console/Program.cs
@@ -1,18 +1,19 @@
-﻿using TupleLibrary;
+﻿using System.IO;
+using TupleLibrary;
 using Sys=System;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        var environment = new Environment(Tuple.Vector(0, -0.1, 0), Tuple.Vector(-0.01, 0, 0));
-        var projectile = new Projectile(Tuple.Point(0, 1, 0), Tuple.Vector(1, 1, 0).Normalize());
+        // var environment = new Environment(Tuple.Vector(0, -0.1, 0), Tuple.Vector(-0.01, 0, 0));
+        // var projectile = new Projectile(Tuple.Point(0, 1, 0), Tuple.Vector(1, 1, 0).Normalize());
 
-        while(projectile.Position.Y >= 0.0)
-        {
-            projectile = Update(environment, projectile);
-            Sys.Console.WriteLine($"x position: {projectile.Position.X}");
-        }
+        // while(projectile.Position.Y >= 0.0)
+        // {
+        //     projectile = Update(environment, projectile);
+        //     Sys.Console.WriteLine($"x position: {projectile.Position.X}");
+        // }
 
         //test saving to canvas
         var c = new Canvas(900, 550);
@@ -27,6 +28,19 @@ internal class Program
         var win = Tuple.Vector(-0.1, 0, 0);
 
         var env = new Environment(grav, win);
+        var red = new Color(1.5, 0, 0);
+
+        while(proj.Position.Y >= 0.0)
+        {
+            proj = Update(env, proj);
+            if(proj.Position.X >= 0 && 
+                proj.Position.X <= 900 &&
+                proj.Position.Y >= 0 &&
+                proj.Position.Y <= 550)
+            c.WritePixel((int)proj.Position.X, (550 - (int)proj.Position.Y), red);
+        }
+        var ppm = c.ToPPM();
+        File.WriteAllText("saved.ppm", ppm);
     }
 
     private static Projectile Update(Environment env, Projectile proj)
