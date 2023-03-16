78ab833eed7c1edd5741d42dba781d3f04471ec0
george-scott-code <george.scott.code@gmail.com>
2023-03-16 23:33:27 +0000

    draw first clock point

--- src/ClockFace.Console/Program.cs
+++ src/ClockFace.Console/Program.cs
@@ -16,7 +16,15 @@ internal partial class Program
 
         //write a pixel to the centre of the canvas
         var red = new Color(1.5, 0, 0);
+        var blue = new Color(0, 0, 1.5);
+
         c.WritePixel((int)start.X, (int)start.Y, red);
+
+        var transform = Matrix.Translation(0, 5, 0);
+
+        var twelve = transform * start;
+        c.WritePixel((int)twelve.X, (int)twelve.Y, blue);
+
         var ppm = c.ToPPM();
         File.WriteAllText("saved.ppm", ppm);
     }
