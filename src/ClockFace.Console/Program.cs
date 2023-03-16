ad500296a52739163261f2d2fbfc8b17777ce0b8
george-scott-code <george.scott.code@gmail.com>
2023-03-16 23:23:31 +0000

    write pixel to centre of canvas

--- src/ClockFace.Console/Program.cs
+++ src/ClockFace.Console/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TupleLibrary;
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        SaveToCanvas();
+    }
+
+    private static void SaveToCanvas()
+    {
+        //test saving to canvas
+        var c = new Canvas(30, 30);
+        var start = TupleLibrary.Tuple.Point(15, 15, 0);
+
+        //write a pixel to the centre of the canvas
+        var red = new Color(1.5, 0, 0);
+        c.WritePixel((int)start.X, (int)start.Y, red);
+        var ppm = c.ToPPM();
+        File.WriteAllText("saved.ppm", ppm);
+    }
+}
