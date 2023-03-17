b496deb5af496e149b94525753f7f9e3ddc40876
george-scott-code <george.scott.code@gmail.com>
2023-03-17 08:38:58 +0000

    rotate hour markings pre refactor

--- src/ClockFace.Console/Program.cs
+++ src/ClockFace.Console/Program.cs
@@ -24,13 +24,23 @@ internal partial class Program
         var transform = Matrix.Translation(0, 0, 1);
 
         var twelve = TupleLibrary.Tuple.Point(0, 0, 1);
-        c.WritePixel((int)twelve.X * radius + 15, (int)twelve.Z * radius + 15, blue);
+        c.WritePixel((int)(twelve.X * radius) + 15, (int)(twelve.Z * radius) + 15, blue);
 
         //rotation
-        var rotation = Matrix.RotationY(3 * (Math.PI/6));
+        var rotation = Matrix.RotationY(1 * (Math.PI/6));
 
         var one = rotation * twelve;
-        c.WritePixel((int)one.X * radius + 15, (int)one.Z * radius + 15, blue);
+        c.WritePixel((int)(one.X * radius) + 15, (int)(one.Z * radius) + 15, blue);
+
+        var rotationTwo = Matrix.RotationY(2 * (Math.PI/6));
+
+        var two = rotationTwo * twelve;
+        c.WritePixel((int)(two.X * radius) + 15, (int)(two.Z * radius) + 15, blue);
+
+        var rotationthree = Matrix.RotationY(3 * (Math.PI/6));
+
+        var three = rotationthree * twelve;
+        c.WritePixel((int)(three.X * radius) + 15, (int)(three.Z * radius) + 15, blue);
 
         var ppm = c.ToPPM();
         File.WriteAllText("saved.ppm", ppm);
