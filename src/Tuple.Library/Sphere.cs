8d1448a71b4cec014a498c76c03dea03cad28f88
george-scott-code <george.scott.code@gmail.com>
2023-03-18 11:56:17 +0000

    featrue: calculate the discriminant of a ray to see if there are intersections

--- src/Tuple.Library/Sphere.cs
+++ src/Tuple.Library/Sphere.cs
@@ -10,6 +10,23 @@ public class Sphere
 
     public double[] Intersection(Ray ray)
     {
-        return new double[0];
+        var discriminant = CalculateDiscriminant(ray);
+        if(discriminant < 0)
+        {
+            return new double[0];
+        }
+        return new double[2];
+    }
+
+    private double CalculateDiscriminant(Ray ray)
+    {
+        // the vector from the sphere's center, to the ray origin
+        // remember: the sphere is centered at the world origin
+        var sphere_to_ray = ray.Origin.Subtract(Tuple.Point(0, 0, 0));
+        var a = ray.Direction.Dot(ray.Direction);
+        var b = 2 * ray.Direction.Dot( sphere_to_ray);
+        var c = sphere_to_ray.Dot( sphere_to_ray) - 1;
+        var discriminant = (b*b) - 4 * a * c;
+        return discriminant;
     }
 }
