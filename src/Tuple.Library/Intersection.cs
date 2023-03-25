59a72c7b3d2b79d562f2de8ff5077bbcd5e1011b
george-scott-code <george.scott.code@gmail.com>
2023-03-25 12:05:04 +0000

    TODO: maintain interseection order

--- src/Tuple.Library/Intersection.cs
+++ src/Tuple.Library/Intersection.cs
@@ -19,6 +19,7 @@ public static class Intersections
     public static Intersection Hit(this Intersection[] intersections)
     {
         // TODO: refactor to list
+        // TODO: maybe better to maintain the order on construction
         var list = intersections.ToList();
         return list.Where(x => x.T > 0).OrderBy(x => x.T).FirstOrDefault();
     }
