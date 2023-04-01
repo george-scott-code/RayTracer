81fd216d38ff5f22e90b1dabaf8f705279ebc240
george-scott-code <george.scott.code@gmail.com>
2023-04-01 10:33:58 +0100

    add ray intersection shadow project

---
+++ src/SphereShadow.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using TupleLibrary;
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        SaveToCanvas();
+    }
+
+    private static void SaveToCanvas()
+    {
+        var width = 100;
+        var height = 100;
+        var c = new Canvas(width, height);
+
+        var ppm = c.ToPPM();
+        File.WriteAllText("saved.ppm", ppm);
+    }
+}
