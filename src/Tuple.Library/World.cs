4578318d51f064259d8c509640d48833f1f205d8
george-scott-code <george.scott.code@gmail.com>
2023-08-12 11:12:55 +0100

    try reverting to unit tests for efficiency

--- src/Tuple.Library/World.cs
+++ src/Tuple.Library/World.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Xunit;
 
 namespace TupleLibrary;
 
@@ -14,8 +15,10 @@ public class World
 
     public static World GetDefaultWorld()
     {
-        var world = new World();
-        world.Light = new PointLight(Tuple.Point(-10, 10, -10), new Color(1, 1, 1));
+        var world = new World
+        {
+            Light = new PointLight(Tuple.Point(-10, 10, -10), new Color(1, 1, 1))
+        };
 
         Material m1 = new(new Color(0.8, 1.0, 0.6), 0.1, 0.7, 0.2, 200.00);
 
@@ -25,5 +28,21 @@ public class World
         world.Objects.Add(new Sphere(transform));
 
         return world;
+    }
+}
+
+// Scenario: Creating a worlds
+// Given w ← world()
+// Then w contains no objects
+// And w has no light source
+
+public class WorldTests
+{
+    [Fact]
+    public void CreatingAWorld()
+    {
+        var world = new World();
+        Assert.Empty(world.Objects);
+        Assert.Null(world.Light);
     }
 }
