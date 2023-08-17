2895d0237b96ee94e5280650b4c641fd7a66edaa
george-scott-code <george.scott.code@gmail.com>
2023-08-17 19:07:11 +0100

    add Arbitrary_view_transformation test

--- src/Tuple.Library/TransformTests.cs
+++ src/Tuple.Library/TransformTests.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using Xunit;
 
 namespace TupleLibrary;
@@ -56,5 +57,35 @@ public class TransformTests
         var t = Matrix.ViewTransform(from, to, up);
         
         Assert.Equivalent(Matrix.Translation(0, 0, -8), t);
+    }
+
+    // Scenario: An arbitrary view transformation
+    // Given from ← point(1, 3, 2)
+    // And to ← point(4, -2, 8)
+    // And up ← vector(1, 1, 0)
+    // When t ← view_transform(from, to, up)
+    // Then t is the following 4x4 matrix:
+    //      | -0.50709 | 0.50709 | 0.67612 | -2.36643 |
+    //      | 0.76772 | 0.60609 | 0.12122 | -2.82843 |
+    //      | -0.35857 | 0.59761 | -0.71714 | 0.00000 |
+    //      | 0.00000 | 0.00000 | 0.00000 | 1.00000 |
+    [Fact]
+    public void Arbitrary_view_transformation()
+    {
+        var from = Tuple.Point(1, 3, 2);
+        var to = Tuple.Point(4, -2, 8);
+        var up = Tuple.Vector(1, 1, 0);
+
+        var t = Matrix.ViewTransform(from, to, up);
+        
+        double[,] elements = new double[4,4]
+        {
+            {-0.50709 , 0.50709 ,  0.67612 , -2.36643 },
+            { 0.76772 , 0.60609 ,  0.12122 , -2.82843 },
+            {-0.35857 , 0.59761 , -0.71714,   0.00000 },
+            { 0.00000 , 0.00000 ,  0.00000 ,  1.00000 },
+        };
+        var expected = new Matrix(elements);
+        Assert.Equivalent(expected, t);
     }
 }
