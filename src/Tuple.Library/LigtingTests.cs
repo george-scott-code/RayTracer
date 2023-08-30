dea27e08f5b5c1fe65bd1aaaf6f529ea3fb5ee16
george-scott-code <george.scott.code@gmail.com>
2023-08-30 20:05:09 +0100

    convert all lighting tests from gherkin

--- src/Tuple.Library/LigtingTests.cs
+++ src/Tuple.Library/LigtingTests.cs
@@ -66,4 +66,44 @@ public class LigtingTests
         var result = Lighting.GetLighting(material, light, position, eyeV, normalV);
         Assert.Equal(new Color(0.7364, 0.7364, 0.7364), result);
     }
+
+    // Scenario: Lighting with eye in the path of the reflection vector
+	// Given a vector (0, -0.70710678118, -0.70710678118) eyev
+	// And a vector (0, 0, -1) normalv
+	// And a point_light(point(0, 10, -10), color(1, 1, 1)) light
+	// When the color result is lighting (m, light, position, eyev, normalv)
+	// Then the color result is color(1.6364, 1.6364, 1.6364)
+    [Fact]
+    public void Lighting_EyeInPathOfReflectionVector()
+    {
+        var material = new Material();
+        var position = Tuple.Point(0,0,0);
+
+        var eyeV = Tuple.Vector(0, -0.70710678118, -0.70710678118);
+        var normalV = Tuple.Vector(0, 0, -1);
+        var light = new PointLight(Tuple.Point(0, 10, -10), new Color(1, 1, 1));
+        
+        var result = Lighting.GetLighting(material, light, position, eyeV, normalV);
+        Assert.Equal(new Color(1.6364, 1.6364, 1.6364), result);
+    }
+
+    // Scenario: Lighting with the light behind the surface
+	// Given a vector (0, 0, -1) eyev
+	// And a vector (0, 0, -1) normalv
+	// And a point_light(point(0, 0, 10), color(1, 1, 1)) light
+	// When the color result is lighting (m, light, position, eyev, normalv)
+	// Then the color result is color(0.1, 0.1, 0.1)
+    [Fact]
+    public void Lighting_LightBehindSurface()
+    {
+        var material = new Material();
+        var position = Tuple.Point(0,0,0);
+
+        var eyeV = Tuple.Vector(0, 0, -1);
+        var normalV = Tuple.Vector(0, 0, -1);
+        var light = new PointLight(Tuple.Point(0, 0, 10), new Color(1, 1, 1));
+        
+        var result = Lighting.GetLighting(material, light, position, eyeV, normalV);
+        Assert.Equal(new Color(0.1, 0.1, 0.1), result);
+    }
 }
