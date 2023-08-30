d9721b1cbb851e8ca82b3a520f90788683b77033
george-scott-code <george.scott.code@gmail.com>
2023-08-30 19:54:50 +0100

    eye offfset lighting test

--- src/Tuple.Library/LigtingTests.cs
+++ src/Tuple.Library/LigtingTests.cs
@@ -26,4 +26,24 @@ public class LigtingTests
         var result = Lighting.GetLighting(material, light, position, eyeV, normalV);
         Assert.Equal(new Color(1.9, 1.9, 1.9), result);
     }
+    
+    // Scenario: Lighting with the eye between light and surface, eye offset 45°
+	// Given a vector (0, 0.707106, -0.707106) eyev
+	// And a vector (0, 0, -1) normalv
+	// And a point_light(point(0, 0, -10), color(1, 1, 1)) light
+	// When the color result is lighting (m, light, position, eyev, normalv)
+	// Then the color result is color(1.0, 1.0, 1.0)
+    [Fact]
+    public void Lighting_EyeBetweenLightAndSurface_Offset()
+    {
+        var material = new Material();
+        var position = Tuple.Point(0,0,0);
+
+        var eyeV = Tuple.Vector(0, 0.707106, -0.707106);
+        var normalV = Tuple.Vector(0, 0, -1);
+        var light = new PointLight(Tuple.Point(0, 0, -10), new Color(1, 1, 1));
+        
+        var result = Lighting.GetLighting(material, light, position, eyeV, normalV);
+        Assert.Equal(new Color(1.0, 1.0, 1.0), result);
+    }
 }
