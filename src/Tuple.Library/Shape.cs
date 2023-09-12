dde98320ee53f5b8740110be2d3aae005729190c
george-scott-code <george.scott.code@gmail.com>
2023-09-12 16:19:45 +0100

    add shape class

---
+++ src/Tuple.Library/Shape.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace TupleLibrary;
+
+public class Shape
+{
+    public Matrix Transform { get; set; }
+    public Material Material { get; set; }
+
+    public Shape()
+    {
+        Transform = Matrix.Identity();
+        Material = new Material();
+    }
+}
