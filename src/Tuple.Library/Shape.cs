16179a496263b1a03804078f825a1d06e3728fdc
george-scott-code <george.scott.code@gmail.com>
2023-09-12 16:33:53 +0100

    add initial tests

--- src/Tuple.Library/Shape.cs
+++ src/Tuple.Library/Shape.cs
@@ -1,4 +1,8 @@
 using System;
+using System.IO.Compression;
+using System.Linq;
+using TupleLibrary.Extensions;
+using Xunit;
 
 namespace TupleLibrary;
 
@@ -11,5 +15,30 @@ public class Shape
     {
         Transform = Matrix.Identity();
         Material = new Material();
+    }
+}
+
+public class ShapeTests
+{
+    // Scenario: The default transformation
+    // Given s ← test_shape()
+    // Then s.transform = identity_matrix
+    [Fact]
+    public void DefaultTransformation()
+    {
+        var shape = new Shape();
+        Assert.Equal(Matrix.Identity(), shape.Transform);
+    }
+
+    // Scenario: Assigning a transformation
+    // Given s ← test_shape()
+    // When set_transform(s, translation(2, 3, 4))
+    // Then s.transform = translation(2, 3, 4)
+    [Fact]
+    public void AssignedTransformation()
+    {
+        var shape = new Shape();
+        shape.Transform = Matrix.Translation(2, 3, 4);
+        Assert.Equal(Matrix.Translation(2, 3, 4), shape.Transform);
     }
 }
